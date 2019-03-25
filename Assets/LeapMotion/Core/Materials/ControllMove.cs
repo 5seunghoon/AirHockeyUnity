25d98a71d3c3ca327bf7cd9971768b7bdb1f5ada
Seunghoon Oh <stdsalmon@gmail.com>
2019-03-25 23:56:10 +0900

    Feat : Socket.io 추가. 스페이스바를 눌릴 때, 그리고 공이 골대 안으로 들어갈 때 Emit함.
    Socket.io for unity : https://github.com/nhnent/socket.io-client-unity3d/

--- Assets/LeapMotion/Core/Materials/ControllMove.cs
+++ Assets/LeapMotion/Core/Materials/ControllMove.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Leap;
 using Leap.Unity;
+using socket.io;
 
 public class ControllMove : MonoBehaviour
 {
 
+    // socket.io for unity : https://github.com/nhnent/socket.io-client-unity3d/
+        
     Controller controller;
     float HandPalmPitch;
     float HandPalmYam;
@@ -14,6 +17,7 @@ public class ControllMove : MonoBehaviour
     float HandWristRot;
 
     string url = "http://127.0.0.1:3000/";
+    public static Socket socket;
 
     void Awake()
     {
@@ -24,6 +28,10 @@ public class ControllMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        socket = Socket.Connect(url);
+        socket.On("HELLO_WORLD", (string data) => {
+            Debug.Log("HELLO_WORLD data : " + data);
+        });
         ballReset();
     }
 
@@ -45,7 +53,9 @@ public class ControllMove : MonoBehaviour
         // 스페이스 바 눌리면 공 위치 초기화
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("space bar down");
             ballReset();
+            socket.Emit("TEST_EVENT", "");
         }
 
 
